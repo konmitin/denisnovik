f19a84b3e0226014b8a3f8cb1ccc5605d0d1c7f8
KonMit <konmitin.work@gmail.com>
2021-03-27 19:15:48 +0300

    Добавил плавную прокрутку по странице. Настроил навигацию по странице. Исправил баг навигационного меню на мобильном разрешении

--- js/main.js
+++ js/main.js
@@ -19,12 +19,62 @@ function changeImage() {
 let menuBurger = document.querySelector('#menuBurger');
 let headerNav = document.querySelector('#headerNav');
 let body = document.querySelector('body');
+let navLink = document.querySelectorAll('.nav__link');
 
 menuBurger.addEventListener('click', () => {
 
+    // Показываю навигационное меню
     menuBurger.classList.toggle('active');
     headerNav.classList.toggle('active');
 
     // Отключил прокрутку страницы
     body.classList.toggle('disable-scroll');
+
+    // "Навесил" навигационным ссылкам обработчик событий
+    navLink.forEach(function(item, i, navLink) {
+
+        item.addEventListener('click', function() {
+
+            // Скрываю навигационное меню
+            menuBurger.classList.remove('active');
+            headerNav.classList.remove('active');
+
+            // Включил прокрутку страницы
+            body.classList.remove('disable-scroll');
+
+        });
+
+    });
+});
+
+// Плавная прокрутка по странице
+
+navLink.forEach(function(item, i, navLink) {
+
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        let href = this.getAttribute('href').substring(1);
+
+        const scrollTarget = document.getElementById(href);
+
+
+        if (scrollTarget) {
+            // const toppOffste = document.querySelector('.scrollto').offsetHeight;
+            const toppOffste = 0; // Если не нужен отсуп сверху
+            const elementPosition = scrollTarget.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - toppOffste;
+
+            window.scroll({
+                top: offsetPosition,
+                behavior: 'smooth',
+            });
+        } else {
+            window.scroll({
+                bottom: e.top,
+                behavior: 'smooth',
+            });
+        }
+    });
+
 });
